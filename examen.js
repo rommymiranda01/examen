bb85110331e5f8288233bd50eb6f29dc65001c03
Rommy Miranda <rommy.miranda@cirvianum.cat>
2021-11-30 16:15:02 +0100

    commit rommy

--- examen.js
+++ examen.js
@@ -1,9 +1,29 @@
 window.onload = function () {
     // Variables
+    var usersArray=[];
+
     let buttPrincipal = document.querySelector('#mainButt');
+    buttPrincipal.addEventListener("click", afegirUsuariPrincipal)
+    let buttSec = document.querySelector('#secButt');
+    buttPrincipal.addEventListener("click", afegirUsuariSec)
+
     //...
     
     // Funciones
-    
+    function afegirUsuariPrincipal() {
+        var nom = window.prompt("Nom: ");
+        var edat = window.prompt("Edat: ");
+        var registre = {'id': 'mainUser', 'nom': nom, 'edat': edat};
+        //usersArray.push(registre);
+        localStorage.setItem(Date.now(), JSON.stringify(registre));
+    }
+
+    function guardarUserPrincipal(){
+        userPrincipal.forEach(user => {
+            userPrincipal.innerHTML += "Nom: "+user.nom+"    "+"Edat:"+user.edat +"<br>";
+        })
+    }
+
+    guardarUserPrincipal();
     // Events
 } 
